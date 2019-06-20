43306c07b94d616a9978dda6e69cb64538a68f79
leogarcia2 <leogarcia2@outlook.fr>
2019-06-20 20:52:55 +0200

    inclure commandes dans installcontroller, editt .env, make bdd, make user, set sitename, set logo, set slogan redirect

--- src/Controller/InstallController.php
+++ src/Controller/InstallController.php
@@ -4,6 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class InstallController extends AbstractController
 {
@@ -15,5 +20,33 @@ class InstallController extends AbstractController
         return $this->render('install/index.html.twig', [
             'controller_name' => 'InstallController',
         ]);
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        if ($request->isMethod('POST')){
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+        	//User creation
+        	$user = new User();
+        	$user->setUsername($_POST['username']);
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $_POST['password']
+                )
+            );
+            $entityManager->persist($user);
+            
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->redirectToRoute('install');
     }
 }
