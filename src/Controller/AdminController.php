bd49113ceaf343f4b296233c37e574039b9fd40b
leogarcia2 <leogarcia2@outlook.fr>
2019-06-26 12:13:27 +0200

    gérer upload image et création template et route à la création de pages

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpKernel\KernelInterface;
 use App\Controller\ConsoleController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AdminController extends AbstractController
 {
@@ -63,7 +64,8 @@ class AdminController extends AbstractController
     		$cC->createEntityForm($kernel, $pageName);
             $cC->fullMigration($kernel);
 
-            //ajout submit type
+            //générer template et route
+
             $formFile = file_get_contents('../src/Form/'.$pageName.'Type.php');
             $formFile = preg_replace("#'data_class' => ".$pageName."::class,#", "'data_class' => ".$pageName."::class,\n            'allow_extra_fields' => true", $formFile);
             file_put_contents('../src/Form/'.$pageName.'Type.php', $formFile);
@@ -79,4 +81,65 @@ class AdminController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/admin/pages/forms/{page}", name="generic_form")
+     */
+    public function generic_form(Request $request, $page)
+    {
+        $classConst = 'App\Entity\\'.$page;
+        $formConst = 'App\Form\\'.$page.'Type';
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository($classConst);
+
+        $entity = $repo->findAll();
+
+        if(!isset($entity[0])){
+            $entity = new $classConst();
+        }else{
+            $entity = $entity[0];
+        }
+
+        $form = $this->createForm($formConst, $entity);
+        $form->add('Save', SubmitType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $entity = $form->getData();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_pages');
+        }
+
+        return $this->render('page/form.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/pages/preview/{page}", name="generic_page")
+     */
+    public function generic_page(Request $request, $page)
+    {
+        $classConst = 'App\Entity\\'.$page;
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository($classConst);
+
+        $entity = $repo->findAll();
+
+        $entity = (array) $entity[0];
+
+        foreach($entity as $name => $field){
+            $oldName = $name;
+            $name = str_replace($classConst, '', $name);
+            $entity[$name] = $entity[$oldName];
+            unset($entity[$oldName]);
+        }
+
+        return $this->render('page/page.html.twig', [
+            'pageName' => $page,
+            'entity' => $entity,
+        ]);
+    }
 }
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -6,69 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PageController extends AbstractController
 {
-    /**
-     * @Route("/admin/pages/forms/{page}", name="generic_form")
-     */
-    public function generic_form(Request $request, $page)
-    {
-    	$classConst = 'App\Entity\\'.$page;
-    	$formConst = 'App\Form\\'.$page.'Type';
-    	$em = $this->getDoctrine()->getManager();
-    	$repo = $em->getRepository($classConst);
-
-    	$entity = $repo->findAll();
-
-    	if(!isset($entity[0])){
-    		$entity = new $classConst();
-    	}else{
-    		$entity = $entity[0];
-    	}
-
-    	$form = $this->createForm($formConst, $entity);
-        $form->add('Save', SubmitType::class);
-
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()){
-            $entity = $form->getData();
-            $em->persist($entity);
-            $em->flush();
-
-            return $this->redirectToRoute('admin_pages');
-        }
-
-        return $this->render('page/form.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/admin/pages/preview/{page}", name="generic_page")
-     */
-    public function generic_page(Request $request, $page)
-    {
-        $classConst = 'App\Entity\\'.$page;
-        $em = $this->getDoctrine()->getManager();
-        $repo = $em->getRepository($classConst);
-
-        $entity = $repo->findAll();
-
-        $entity = (array) $entity[0];
-
-        foreach($entity as $name => $field){
-            $oldName = $name;
-            $name = str_replace($classConst, '', $name);
-            $entity[$name] = $entity[$oldName];
-            unset($entity[$oldName]);
-        }
-
-        return $this->render('page/page.html.twig', [
-            'pageName' => $page,
-            'entity' => $entity,
-        ]);
-    }
+    
 }
