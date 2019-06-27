f16de592b3447f3a35466a7400a0b7e91d35bbac
leogarcia2 <leogarcia2@outlook.fr>
2019-06-27 09:48:04 +0200

    start file upload

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -7,4 +7,20 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
+    /**
+     * @Route("/test", name="test")
+     */
+    public function Test()
+    {
+        $classConst = 'App\Entity\Test';
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository($classConst);
+        $entity = $repo->findAll();
+        $entity = $entity[0];
+
+        return $this->render('theme/pages/test.html.twig', [
+            'page' => $entity,
+        ]);
+    }
+    
 }
