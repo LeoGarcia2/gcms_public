35c0605e4f47556121826f2cf9063f0e53bd4126
leogarcia2 <leogarcia2@outlook.fr>
2019-07-15 15:58:29 +0200

    route for single entry ok

--- src/Controller/EntriesController.php
+++ src/Controller/EntriesController.php
@@ -6,16 +6,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Knp\Component\Pager\PaginatorInterface;
 
 class EntriesController extends AbstractController
 {
     /**
      * @Route("/{ctWithoutCT}", name="listing")
      */
-    public function listing(Request $request, $ctWithoutCT)
+    public function listing(Request $request, $ctWithoutCT, PaginatorInterface $paginator)
     {
-        return new Response($ctWithoutCT);
+        $ct = 'CT'.$ctWithoutCT;
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('App\Entity\\'.$ct);
+
+        $allEntriesQuery = $repo->createQueryBuilder('e')
+            ->where('e.published == :published')
+            ->setParameter('published', true)
+            ->getQuery();
+
+        $entries = $paginator->paginate(
+            $allEntriesQuery,
+            $request->query->getInt('page', 1),
+            20
+        );
+
+        return $this->render('theme/entries/'.strtolower($ct).'/listing.html.twig', [
+            'ct' => $ct,
+            'ctWithoutCT' => $ctWithoutCT,
+            'entries' => $entries,
+        ]);
     }
+
     /**
      * @Route("/{ctWithoutCT}/{id}", name="entry", requirements={"id"="\d+"})
      */
