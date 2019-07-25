d6fce72d7c8989ebcc36ae68b2fd163809a90b2f
leogarcia2 <leogarcia2@outlook.fr>
2019-07-25 15:22:59 +0200

    only users management and rights to do

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -9,4 +9,21 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class PageController extends AbstractController
 {
+    //pageaaastart
+    /**
+     * @Route("/", name="pageaaa")
+     */
+    public function PageAaa()
+    {
+        $classConst = 'App\Entity\PageAaa';
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository($classConst);
+        $entity = $repo->findByPublished(true);
+        $entity = $entity[0];
+
+        return $this->render('theme/pages/pageaaa.html.twig', [
+            'page' => $entity,
+        ]);
+    }//pageaaaend
+
 }
